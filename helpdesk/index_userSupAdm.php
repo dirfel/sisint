dae84481f2d9384d5f2b7809124fd378b4001c4b
dirfel <dirceufelipef@hotmail.com>
2023-12-28 17:39:19 -0400

    Adicionado readme (ainda incompleto) e alterado links do helpdesk

--- helpdesk/index_userSupAdm.php
+++ helpdesk/index_userSupAdm.php
@@ -1,10 +1,10 @@
 <div class="col-sm-12 col-md-12">
   <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('https://atendimento.6cta.eb.mil.br/otobo/customer.pl', 'Serviço HelpDesk 6CTA', 'icon fa fa-laptop', 
-      '<h4 class="subtitle"><b>Atendimento 6º CTA</b></h4><h1 class="title">HelpDesk 6CTA</h1>', '_blank', '' ); ?></div>
+    <?php render_card2('https://atendimento.xcta.eb.mil.br/otobo/customer.pl', 'Serviço HelpDesk xCTA', 'icon fa fa-laptop', 
+      '<h4 class="subtitle"><b>Atendimento 6º CTA</b></h4><h1 class="title">HelpDesk xCTA</h1>', '_blank', '' ); ?></div>
         
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.103.2/intranet/administrator/', 'Serviço Intranet', 'icon fab fa-joomla', 
+    <?php render_card2('http://10.10.10.10.2/intranet/administrator/', 'Serviço Intranet', 'icon fab fa-joomla', 
       '<h4 class="subtitle"><b>Administrador INTRAnet</b></h4><h1 class="title">INTRAnet</h1>', '_blank', '' ); ?></div>
         
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
@@ -12,47 +12,47 @@
       '<h4 class="subtitle"><b>Administrador INTERnet</b></h4><h1 class="title">INTERnet</h1>', '_blank', '' ); ?></div>
 
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.100.2:19999/', 'Servidor Squid', 'icon fas fa-wave-square', 
+    <?php render_card2('http://10.10.10.10.2:19999/', 'Servidor Squid', 'icon fas fa-wave-square', 
       '<h4 class="subtitle"><b>Netdata System Overview</b></h4><h1 class="title">Squid</h1>', '_blank', '' ); ?></div>
         
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.100.2:19999/', 'Servidor Squid', 'icon fas fa-wave-square', 
+    <?php render_card2('http://10.10.10.10.2:19999/', 'Servidor Squid', 'icon fas fa-wave-square', 
       '<h4 class="subtitle"><b>Netdata System Overview</b></h4><h1 class="title">Squid</h1>', '_blank', '' ); ?></div>
 
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.103.2:19999/', 'Servidor Web', 'icon fas fa-wave-square', 
+    <?php render_card2('http://10.10.10.10.2:19999/', 'Servidor Web', 'icon fas fa-wave-square', 
       '<h4 class="subtitle"><b>Netdata System Overview</b></h4><h1 class="title">Web</h1>', '_blank', '' ); ?></div>
 
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.103.3:19999/', 'Servidor Inventário', 'icon fas fa-wave-square', 
+    <?php render_card2('http://10.10.10.10.3:19999/', 'Servidor Inventário', 'icon fas fa-wave-square', 
       '<h4 class="subtitle"><b>Netdata System Overview</b></h4><h1 class="title">Inventário</h1>', '_blank', '' ); ?></div>
 
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.103.13:19999/', 'Servidor SPED', 'icon fas fa-wave-square', 
+    <?php render_card2('http://10.10.10.10.13:19999/', 'Servidor SPED', 'icon fas fa-wave-square', 
       '<h4 class="subtitle"><b>Netdata System Overview</b></h4><h1 class="title">SPED</h1>', '_blank', '' ); ?></div>
 
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.103.20:19999/', 'Servidor ARQUIVOS', 'icon fas fa-wave-square', 
+    <?php render_card2('http://10.10.10.10.20:19999/', 'Servidor ARQUIVOS', 'icon fas fa-wave-square', 
       '<h4 class="subtitle"><b>Netdata System Overview</b></h4><h1 class="title">ARQUIVOS</h1>', '_blank', '' ); ?></div>
 
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('https://10.56.84.20/lam//templates/login.php?useProfile=3biaaaae', 'Servidor LDAP 6CTA', 'icon fas fa-cloud', 
+    <?php render_card2('https://10.10.10.10/lam//templates/login.php?useProfile=omnome', 'Servidor LDAP 6CTA', 'icon fas fa-cloud', 
       '<h4 class="subtitle"><b>Administrador LDAP EBNet</b></h4><h1 class="title">LDAP 6CTA</h1>', '_blank', '' ); ?></div>
     
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('https://correio.3biaaaae.eb.mil.br:9071/zimbraAdmin/', 'Serviço Zimbra', 'icon fas fa-mail-bulk', 
+    <?php render_card2('https://correio.omnome.eb.mil.br:9071/zimbraAdmin/', 'Serviço Zimbra', 'icon fas fa-mail-bulk', 
       '<h4 class="subtitle"><b>Administrador Zimbra</b></h4><h1 class="title">Zimbra</h1>', '_blank', '' ); ?></div>
     
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.103.2/phpmyadmin/', 'Servidor WEB', 'icon fa fa-database', 
+    <?php render_card2('http://10.10.10.10.2/phpmyadmin/', 'Servidor WEB', 'icon fa fa-database', 
       '<h4 class="subtitle"><b>Database</b></h4><h1 class="title">WEB</h1>', '_blank', '' ); ?></div>
     
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.100.2/sarg/', 'Servidor Squid', 'icon fas fa-file-alt', 
+    <?php render_card2('http://10.10.10.10.2/sarg/', 'Servidor Squid', 'icon fas fa-file-alt', 
       '<h4 class="subtitle"><b>Relalório de Acessos</b></h4><h1 class="title">Squid</h1>', '_blank', '' ); ?></div>
     
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.100.2/squidanalyzer/', 'Servidor Squid', 'icon fas fa-file-signature', 
+    <?php render_card2('http://10.10.10.10.2/squidanalyzer/', 'Servidor Squid', 'icon fas fa-file-signature', 
       '<h4 class="subtitle"><b>SquidAnalyzer</b></h4><h1 class="title">Squid</h1>', '_blank', '' ); ?></div>
 
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
@@ -60,7 +60,7 @@
       '<h4 class="subtitle"><b>Webmin</b></h4><h1 class="title">Squid</h1>', '_blank', '' ); ?></div>
     
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('https://10.57.100.2:10000/', 'Servidor Squid', 'icon fab fa-first-order', 
+    <?php render_card2('https://10.10.10.2:10000/', 'Servidor Squid', 'icon fab fa-first-order', 
       '<h4 class="subtitle"><b>Webmin</b></h4><h1 class="title">Squid</h1>', '_blank', '' ); ?></div>
 
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
@@ -72,25 +72,25 @@
         <span class="input-group-addon"><button type="submit" class="fa fa-check color-adm"></button></span></form>', '_blank', '' ); ?></div></div>
 
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('https://10.57.100.2:10000/', 'Servidor Squid', 'icon fab fa-first-order', 
+    <?php render_card2('https://10.10.10.2:10000/', 'Servidor Squid', 'icon fab fa-first-order', 
       '<h4 class="subtitle"><b>Webmin</b></h4><h1 class="title">Squid</h1>', '_blank', '' ); ?></div>
     
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('https://10.57.103.1/ui/#/login', 'Servidor R710', 'icon fa fa-server', 
+    <?php render_card2('https://10.10.10.1/ui/#/login', 'Servidor R710', 'icon fa fa-server', 
       '<h4 class="subtitle"><b>Servidores Virtualizados I</b></h4><h1 class="title">R710</h1>', '_blank', '' ); ?></div>
     
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('https://10.57.103.11/', 'Servidor DL380 G5', 'icon fa fa-server', 
+    <?php render_card2('https://10.10.10.10/', 'Servidor DL380 G5', 'icon fa fa-server', 
       '<h4 class="subtitle"><b>Servidores Virtualizados II</b></h4><h1 class="title">DL380 G5</h1>', '_blank', '' ); ?></div>
     
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('http://10.57.103.3/ocsreports/', 'Servidor Inventário', 'icon fa fa-list-alt', 
+    <?php render_card2('http://10.10.10.10.3/ocsreports/', 'Servidor Inventário', 'icon fa fa-list-alt', 
       '<h4 class="subtitle"><b>OCS inventory</b></h4><h1 class="title">Inventário</h1>', '_blank', '' ); ?></div>
     
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('https://10.57.103.160/', 'Gravador NVR 7132', 'icon fa fa-video-camera', 
+    <?php render_card2('https://10.57.10.10/', 'Gravador NVR 7132', 'icon fa fa-video-camera', 
       '<h4 class="subtitle"><b>Gravador digital de vídeo em rede</b></h4><h1 class="title">NVR 7132</h1>', '_blank', '' ); ?></div>
     
     <div class="col-sm-12 col-lg-4 animated fadeInRightBig">
-    <?php render_card2('https://kanbanflow.com/board/5RoN2oX', 'Servidor Inventário', 'icon fas fa-bookmark', 
+    <?php render_card2('https://kanbanflow.com/board/5h7o2oX', 'Servidor Inventário', 'icon fas fa-bookmark', 
       '<h4 class="subtitle"><b>Controle das entradas e saídas</b></h4><h1 class="title">KanbanFlow</h1>', '_blank', '' ); ?></div>
---
+++ readme.md
@@ -0,0 +1,182 @@
+# Sistemas Integrados de Organização Militar
+
+Os Sistemas Integrados (SISINT) é um portal que engloba diversos sistemas para o apoio a rotinas de uma organização militar.
+Foi criado inicialmente para atender a 3ª Bateria de Artilharia Antiaérea, mas com alguns ajustes, agora é possível atender muito mais OM do Exército.
+Por ser um sistema Web, o usuário final pode acessar usando um computador conectado na Rede Corporativa. Não possui custo operacional, pois o sistema pode funcionar no mesmo servidor da página da Intranet da OM.
+
+
+## Visão Geral
+
+Esse projeto contempla os seguintes serviços:
+
+- Arranchamento
+    - Permite o arranchamento individual e coletivo
+    - Gera relatório de arranchamento
+    - Permite o cadastro de cardápio
+
+- Controle de FO de Cb/Sd
+    - Cadastros e elaboração de relatórios individuais e coletivos
+
+- Helpdesk para a Seção de TI
+    - Permite troca de mensagem para solicitação de serviços
+    - Permite consulta do status do pedido
+
+- Sistema de controle de cautelas do Encarregado de Material
+    - Permite o cadastro e controle do material cadastrado
+    - O militar pode consultar o material que tem cautelado nos depósitos
+    - O Enc Mat pode cadastrar auxiliares
+    - Pode gerar relatórios por militar, por material ou geral
+
+- Gestão de hóspedes ao hotel de trânsito
+    - controle visual de quais reservas foram feitas
+    - calendário mensal de hospedes para impressão
+
+- Agenda
+    - agenda visual
+    - os eventos podem ser cadastrados como privados, públicos ou compartimentados com um usuário de escolha
+    - permite o envio de anexos ao evento (ex: Ordem de Instrução)
+
+- Controle de Pessoal
+    - Livro de Afastamento da Guarnição
+    - Cadastro/Desativação/Modificação do Público interno
+    - Relatório do plano de chamada com link para conversa no Whatsapp e busca de endereço no google maps
+    - Ficha do Cadben para impressão
+    - Controle do quantitativo do efetivo existente
+
+- Guarda
+    - Livro de Partes do Of Dia
+    - Controle de entrada e saída de:
+        - Visitantes / veículos
+        - Militares
+        - Viaturas
+        - Rondas / permanências
+        - Quartos de hora
+        - Armamento
+- Outros
+    - Consulta a Boletins Internos da OM (Importado do SISBOL)
+    - Obriga o usuário a atualizar seus dados
+    - Obriga o usuário a manter senha forte e bloqueia por um dia o usuário que errar a senha 10 vezes consecutivas
+    - gera logs de quase todos os eventos, permitindo auditoria dos dados
+    - permite a criação de backup
+
+## Pré-requisitos
+
+São requisitos para o sistema funcionar corretamente:
+
+- Apache2
+- PHP 7.2 ou superior (recomendado 8+)
+- MariaDB e PHPMyAdmin
+- 1 Gb de armazenamento livre (Recomendado 10+ Gb)
+- Memória RAM de 4 GB (Recomendado 8+ Gb)
+
+## Instalação
+
+1. Com os requisitos de Software acima, navegue até o diretório:
+
+```bash
+cd /var/www/html/
+```
+
+2. Clone este repositório:
+
+```bash
+git clone https://github.com/dirfel/sisint.git
+```
+
+3. Navegue até o diretório do projeto:
+
+```bash
+cd sisint
+```
+
+4. Altere as informações do Sistema:
+
+```bash
+nano ./recursos/models/conexao.php   # pode ser feito através do VS Code ou outro editor
+```
+
+4. Configurações adicionais, sugeridas:
+
+- Alterar o arquivo ./recursos/assets/favicon.png pelo ícone desejado (Recomendo que seja no mesmo tamanho); e
+- Alterar o arquivo ./recursos/assets/wallpaper.png por uma imagem de sua OM para que possa compor o plano de fundo da tela de login.
+- Alterar os serviços e links no arquivo helpdesk/index_userSupAdm.php
+
+## Configuração
+
+1. Abra o arquivo de configuração:
+
+    ```bash
+    nano recursos/models/conexao.php
+    ```
+
+2. Edite as configurações conforme necessário.
+    - A variável test deve estar em true somente em servidor de desenvolvimento e testes.
+
+## Acesso inicial
+
+1. Inicie a aplicação:
+
+```bash
+service apache2 start
+```
+
+2. Abra o navegador e acesse usando o link http://endereco/sisint , aqui deve aparecer ua tela de login.
+    - IMPORTANTE: teste também se o arquivo http://endereco/sisint/recursos/models/conexao.php ou http://endereco/sisint/recursos/models/conexao.php~ não exibe a senha do banco de dados publicamente, caso esteja, altere as configurações do Apache.
+
+3. Acesse o endereço http://endereco/phpmyadmin (realize o login se for necessário)
+    - Importe o arquivo database.sql
+
+3. Realize o login com a conta pré-cadstrada:
+    CPF: 12312312300
+    Senha: 12312312300
+
+## Capturas de Tela
+
+![Captura de Tela 1](capturas/Captura1.png)
+Você será solicitado a trocar de senha e mais tarde para atualizar seus dados do Plano de Chamada, siga as instruções para cadastrar uma senha forte.
+
+Vamos começar a ver como é o nosso sistema:
+
+![Captura de Tela 2](capturas/Captura2.png)
+No painel superior você pode verificar em qual módulo do sistema você está.
+
+![Captura de Tela 3](capturas/Captura3.png)
+Passando o mouse sobre ícones podemos ver a dica da sua função. Na imagem estão os botões de logout, tela inicial e reportar erro. Clicamos sobre a engrenagem.
+
+![Captura de Tela 2](capturas/Captura4.png)
+Descrição opcional da segunda captura de tela.
+
+![Captura de Tela 2](capturas/Captura5.png)
+Descrição opcional da segunda captura de tela.
+
+![Captura de Tela 2](capturas/Captura6.png)
+Descrição opcional da segunda captura de tela.
+
+![Captura de Tela 2](capturas/Captura7.png)
+Descrição opcional da segunda captura de tela.
+
+![Captura de Tela 2](capturas/Captura8.png)
+Descrição opcional da segunda captura de tela.
+
+![Captura de Tela 2](capturas/Captura9.png)
+Descrição opcional da segunda captura de tela.
+
+![Captura de Tela 2](capturas/Captura10.png)
+Descrição opcional da segunda captura de tela.
+
+![Captura de Tela 2](capturas/Captura11.png)
+Descrição opcional da segunda captura de tela.
+
+## Contribuição
+
+Se quiser contribuir, siga os passos abaixo:
+
+1. Faça um fork do projeto.
+2. Crie uma branch para sua feature: `git checkout -b feature-nova`.
+3. Faça commit das suas alterações: `git commit -m 'Adicione uma nova feature'`.
+4. Faça push para a branch: `git push origin feature-nova`.
+5. Abra um pull request.
+
+## Licença
+
+Este projeto é licenciado sob a [Sua Licença](LICENSE).
